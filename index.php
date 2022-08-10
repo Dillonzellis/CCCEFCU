d856974ea0b3dfc8c42de8a80f5368abb81a8f45
Dillon Ellis <dellis@fcul.com>
2022-08-10 10:09:17 -0400

    Add stylesheet and reduce motion query

--- index.php
+++ index.php
@@ -1,64 +1,66 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="en-US">
+
 <head>
 
-	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta name="author" content="SemiColonWeb" />
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="author" content="SemiColonWeb" />
 
-	<!-- Stylesheets
+    <!-- Stylesheets
 	============================================= -->
-	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
-	<link rel="stylesheet" href="style.css" type="text/css" />
-	<link rel="stylesheet" href="css/dark.css" type="text/css" />
-	<link rel="stylesheet" href="css/font-icons.css" type="text/css" />
-	<link rel="stylesheet" href="css/animate.css" type="text/css" />
-	<link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
-	<link rel="stylesheet" href="css/swiper.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400i,700|Poppins:300,400,500,600,700|PT+Serif:400,400i&display=swap" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" href="css/dark.css" type="text/css" />
+    <link rel="stylesheet" href="css/font-icons.css" type="text/css" />
+    <link rel="stylesheet" href="css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+    <link rel="stylesheet" href="css/swiper.css" type="text/css">
 
-	<link rel="stylesheet" href="css/custom.css" type="text/css" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" href="js/masterslider/style/masterslider.css" />
+    <link rel="stylesheet" href="css/custom.css" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="js/masterslider/style/masterslider.css" />
+    <link rel="stylesheet" href="./css/style-de.css">
 
-	<!-- Document Title
+    <!-- Document Title
 	============================================= -->
-	<title>Coweta - Home</title>
+    <title>Coweta - Home</title>
 
 </head>
 
 <body class="stretched">
 
-	<!-- Document Wrapper
+    <!-- Document Wrapper
 	============================================= -->
-	<div id="wrapper" class="clearfix">
-		
-		<p align="center" style="background: #CEDB00; padding: 15px; font-size: 18px; color:#000000;"><strong>GENERAL NOTICE TO MEMBERSHIP.  A VERIFICATION OF MEMBERS’ ACCOUNTS IS BEING PERFORMED AS OF JUNE 30, 2022.  ALL MEMBERS SHOULD RECEIVE A STATEMENT EITHER BY MAIL OR E-STATEMENT.  PLEASE COMPARE YOUR STATEMENT WITH YOUR RECORDS AND REPORT ANY DIFFERENCES IN BALANCES TO: CORNERSTONE RESOURCES
-PO BOX 655147
-DALLAS, TX 75265-5147
-</a></strong></p>
+    <div id="wrapper" class="clearfix">
 
-		<!-- Header
+        <p align="center" style="background: #CEDB00; padding: 15px; font-size: 18px; color:#000000;"><strong>GENERAL NOTICE TO MEMBERSHIP. A VERIFICATION OF MEMBERS’ ACCOUNTS IS BEING PERFORMED AS OF JUNE 30, 2022. ALL MEMBERS SHOULD RECEIVE A STATEMENT EITHER BY MAIL OR E-STATEMENT. PLEASE COMPARE YOUR STATEMENT WITH YOUR RECORDS AND REPORT ANY DIFFERENCES IN BALANCES TO: CORNERSTONE RESOURCES
+                PO BOX 655147
+                DALLAS, TX 75265-5147
+                </a></strong></p>
+
+        <!-- Header
 		============================================= -->
-		<header id="header" class="full-header transparent-header dark">
-		
-		<?php include 'include/header.php';?>
-		</header><!-- #header end -->
-		<section id="slider" class="slider-element slider-parallax swiper_wrapper include-header slider-parallax-visible" style="margin-top: -130px;">
-			<div class="slider-inner" style="transform: translate3d(0px, -360px, 0px); height: 867px;">
+        <header id="header" class="full-header transparent-header dark">
 
-				<div class="swiper-container swiper-parent swiper-container-initialized swiper-container-horizontal" style="cursor: grab;">
-					<div class="swiper-wrapper" style="transition: all 0ms ease 0s; transform: translate3d(0px, 0px, 0px);">
-						<div class="swiper-slide dark swiper-slide-active" style="width: 1903px;">
-							<div class="container">
-								<div class="slider-caption slider-caption-left" style="transform: translate3d(0px, -100px, 0px); opacity: -0.530507;">
-									<h2 data-animate="fadeInUp" class="fadeInUp animated">Join Coweta Cities & County EFCU<br>We make it easy!</h2>
-									<p class="d-none d-sm-block fadeInUp animated" data-animate="fadeInUp" data-delay="200">Members get benefits banks can't match.<br>Switching to Coweta Cities & County EFCU is easy!</p>
-									<a href="who-can-join.php" class="button button-border button-light button-rounded button-reveal text-right button-large d-md-inline-block"><i class="icon-angle-right"></i><span>Why Join?</span></a>
-								</div>
-							</div>
-							<div class="swiper-slide-bg" style="background-image: url('images/landing/teacherBanner.jpg');"></div>
-						</div>
-						<!--<div class="swiper-slide dark swiper-slide-active" style="width: 1903px;">
+            <?php include 'include/header.php'; ?>
+        </header><!-- #header end -->
+        <section id="slider" class="slider-element slider-parallax swiper_wrapper include-header slider-parallax-visible" style="margin-top: -130px;">
+            <div class="slider-inner" style="transform: translate3d(0px, -360px, 0px); height: 867px;">
+
+                <div class="swiper-container swiper-parent swiper-container-initialized swiper-container-horizontal" style="cursor: grab;">
+                    <div class="swiper-wrapper" style="transition: all 0ms ease 0s; transform: translate3d(0px, 0px, 0px);">
+                        <div class="swiper-slide dark swiper-slide-active" style="width: 1903px;">
+                            <div class="container">
+                                <div class="slider-caption slider-caption-left" style="transform: translate3d(0px, -100px, 0px); opacity: -0.530507;">
+                                    <h2 data-animate="fadeInUp" class="fadeInUp animated">Join Coweta Cities & County EFCU<br>We make it easy!</h2>
+                                    <p class="d-none d-sm-block fadeInUp animated" data-animate="fadeInUp" data-delay="200">Members get benefits banks can't match.<br>Switching to Coweta Cities & County EFCU is easy!</p>
+                                    <a href="who-can-join.php" class="button button-border button-light button-rounded button-reveal text-right button-large d-md-inline-block"><i class="icon-angle-right"></i><span>Why Join?</span></a>
+                                </div>
+                            </div>
+                            <div class="swiper-slide-bg" style="background-image: url('images/landing/teacherBanner.jpg');"></div>
+                        </div>
+                        <!--<div class="swiper-slide dark swiper-slide-active" style="width: 1903px;">
 							<div class="container">
 								<div class="slider-caption slider-caption-left" style="transform: translate3d(0px, -135px, 0px); opacity: -0.530507;">
 									<h2 data-animate="fadeInUp" class="fadeInUp animated">JOIN COWETA EFCU<br>WE MAKE IT EASY</h2>
@@ -68,122 +70,129 @@ DALLAS, TX 75265-5147
 							</div>
 							<div class="swiper-slide-bg" style="background-image: url('images/landing/feetSandBanner.jpg');"></div>
 						</div>-->
-					</div>
-					<div class="slider-arrow-left swiper-button-disabled" style="opacity: -0.530507;" tabindex="-1" role="button" aria-label="Previous slide" aria-disabled="true"><i class="icon-angle-left"></i></div>
-					<div class="slider-arrow-right" style="opacity: -0.530507;" tabindex="0" role="button" aria-label="Next slide" aria-disabled="false"><i class="icon-angle-right"></i></div>
-				<span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
+                    </div>
+                    <div class="slider-arrow-left swiper-button-disabled" style="opacity: -0.530507;" tabindex="-1" role="button" aria-label="Previous slide" aria-disabled="true"><i class="icon-angle-left"></i></div>
+                    <div class="slider-arrow-right" style="opacity: -0.530507;" tabindex="0" role="button" aria-label="Next slide" aria-disabled="false"><i class="icon-angle-right"></i></div>
+                    <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+                </div>
 
-				<a href="#" data-scrollto="#content" data-offset="100" class="one-page-arrow dark"><i class="icon-angle-down infinite animated fadeInDown"></i></a>
+                <a href="#" data-scrollto="#content" data-offset="100" class="one-page-arrow dark"><i class="icon-angle-down infinite animated fadeInDown"></i></a>
 
-			</div>
-			
-		</section>
-		
-		
-			</div> 
-	<div id="quick-links">
-				<div class="container">
-					<div class="row">
-						<div class="col-lg-4 col-4 quickLink">
-							<a href="auto-loans.php"><img src="images/car-loan-bulb.png" alt="loan icon"><br><span>Car Loans</span></a>
-						</div>
-						
-						<div class="col-lg-4 col-4 quickLink">
-							<a href="personal.php"><img src="images/personal-loan-bulb.png" alt="online banking icon"><br><span>Personal Loans</span></a>
-						</div>
-						
-						<div class="col-lg-4 col-4 quickLink">
-							<a href="#membership" data-lightbox="inline"><img src="images/membership-bulb.png" alt="newsletter icon"><br><span>Membership</span></a>
-						</div>
-						
-						
-					</div>
-					
-				</div>
-			</div>
-	<!--<svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="-300 0 950 270" >
+            </div>
+
+        </section>
+
+
+    </div>
+    <div id="quick-links">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-4 quickLink">
+                    <a href="auto-loans.php"><img src="images/car-loan-bulb.png" alt="loan icon"><br><span>Car Loans</span></a>
+                </div>
+
+                <div class="col-lg-4 col-4 quickLink">
+                    <a href="personal.php"><img src="images/personal-loan-bulb.png" alt="online banking icon"><br><span>Personal Loans</span></a>
+                </div>
+
+                <div class="col-lg-4 col-4 quickLink">
+                    <a href="#membership" data-lightbox="inline"><img src="images/membership-bulb.png" alt="newsletter icon"><br><span>Membership</span></a>
+                </div>
+
+
+            </div>
+
+        </div>
+    </div>
+    <!--<svg id="svg" xmlns="http://www.w3.org/2000/svg" viewBox="-300 0 950 270" >
     <path d="M-314,267 C105,364 400,100 812,279" fill="none" stroke="white" stroke-width="120" stroke-linecap="round"/>
   </svg>-->
-			
-<!--
+
+    <!--
 		=============================================
 Dividerrrrrrrrrrr
 		=============================================-->
-		<div class="section border-0 mb-0">
-					<div class="container clearfix">
+    <div class="section border-0 mb-0">
+        <div class="container clearfix">
 
-						<div class="heading-block center mb-0">
-							<h2>"JOIN <span style="color: #0e004e;">COWETA CITIES & COUNTY EFCU</span>...WE MAKE IT EASY!"</h2>
-						</div>
+            <div class="heading-block center mb-0">
+                <h2>"JOIN <span style="color: #0e004e;">COWETA CITIES & COUNTY EFCU</span>...WE MAKE IT EASY!"</h2>
+            </div>
 
-					</div>
-				</div>
-		<!--END Dividerrrrrrrrrrr
+        </div>
+    </div>
+    <!--END Dividerrrrrrrrrrr
 		=============================================-->
-		<!-- Content
+    <!-- Content
 		============================================= -->
-		<section id="content" style="background: url('images/coweta-bg.jpg') no-repeat; background-size: cover;">
-			<div class="content-wrap cowFacts">
-				<div class="container clearfix" align="center">
-				
-					<h2 style="font-size: 2.7vw;">Coweta Cities & County EFCU<br>
-						Your Immediate Family</h2>
-				<p style="font-size: 1.7vw;">The mission of Coweta Cities and County Employees Federal Credit Union is to provide quality, low cost financial services to our members.</p>
-					<a href="about.php" class="button button-border button-light button-rounded button-reveal text-right button-large d-md-inline-block" ><i class="icon-angle-right"></i><span>Learn More</span></a>
+    <section id="content" style="background: url('images/coweta-bg.jpg') no-repeat; background-size: cover;">
+        <div class="content-wrap cowFacts">
+            <div class="container clearfix" align="center">
 
-				</div>
+                <h2 style="font-size: 2.7vw;">Coweta Cities & County EFCU<br>
+                    Your Immediate Family</h2>
+                <p style="font-size: 1.7vw;">The mission of Coweta Cities and County Employees Federal Credit Union is to provide quality, low cost financial services to our members.</p>
+                <a href="about.php" class="button button-border button-light button-rounded button-reveal text-right button-large d-md-inline-block"><i class="icon-angle-right"></i><span>Learn More</span></a>
 
-				
+            </div>
 
-				
-				
 
-			</div>
-		</section><!-- #content end -->
 
-		
-		<?php include 'include/footer.php';?>
 
-	</div><!-- #wrapper end -->
 
-	<!-- Go To Top
+
+        </div>
+    </section><!-- #content end -->
+
+
+    <?php include 'include/footer.php'; ?>
+
+    </div><!-- #wrapper end -->
+
+    <!-- Go To Top
 	============================================= -->
-	<div id="gotoTop" class="icon-angle-up"></div>
+    <div id="gotoTop" class="icon-angle-up"></div>
 
-	<!-- JavaScripts
+    <!-- JavaScripts
 	============================================= -->
-	<script src="js/jquery.js"></script>
-	<script src="js/plugins.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/plugins.min.js"></script>
 
-	<!-- Footer Scripts
+    <!-- Footer Scripts
 	============================================= -->
-	<script src="js/functions.js"></script>
-	<script src="js/masterslider/masterslider.min.js"></script> 
-		<script>
-(function($) {
- "use strict";
-	var slider = new MasterSlider();
-	// adds Arrows navigation control to the slider.
-	slider.control('arrows');
-	slider.control('bullets');
-	
-	slider.setup('masterslider' , {
-		 width:1600,    // slider standard width
-		 height:600,   // slider standard height
-		 space:0,
-		 speed:45,
-		 loop:true,
-		 preload:0,
-		 autoplay:true,
-		 view:"parallaxMask"
-	});
-})(jQuery);
-</script>
-	<script>
-	$(function() {
-		$( "#side-navigation" ).tabs({ show: { effect: "fade", duration: 400 } });
-	});
-</script>
+    <script src="js/functions.js"></script>
+    <script src="js/masterslider/masterslider.min.js"></script>
+    <script>
+        (function($) {
+            "use strict";
+            var slider = new MasterSlider();
+            // adds Arrows navigation control to the slider.
+            slider.control('arrows');
+            slider.control('bullets');
+
+            slider.setup('masterslider', {
+                width: 1600, // slider standard width
+                height: 600, // slider standard height
+                space: 0,
+                speed: 45,
+                loop: true,
+                preload: 0,
+                autoplay: true,
+                view: "parallaxMask"
+            });
+        })(jQuery);
+    </script>
+    <script>
+        $(function() {
+            $("#side-navigation").tabs({
+                show: {
+                    effect: "fade",
+                    duration: 400
+                }
+            });
+        });
+    </script>
 
 </body>
+
 </html>
